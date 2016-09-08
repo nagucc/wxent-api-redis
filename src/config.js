8eca5d4f69d83553eb9e3287fc50863eff4e4009
na57 <na57@qq.com>
2016-09-08 08:59:55 +0800

    roll back

--- src/config.js
+++
@@ -1,4 +0,0 @@
-
-export const port = process.env.PORT || 3000;
-
-export const host = process.env.WEBSITE_HOSTNAME || `localhost:${port}`;
--- src/index.js
+++ src/index.js
@@ -4,31 +4,5 @@ eslint-disable no-console
  */
 
 import 'babel-polyfill';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import { host, port } from './config';
-import route from './route';
 
-const app = express();
-
-//
-// Register Node.js middleware
-// -----------------------------------------------------------------------------
-app.use(cookieParser('my cookie key'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-const morgan = require('morgan');
-app.use(morgan('dev'));
-
-/*
-注册API
- */
-app.use('/route', route);
-
-
-app.listen(port, () => {
-  console.log(`The server is running at http://${host}/`);
-});
+console.log('hello');
